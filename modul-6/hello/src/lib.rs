58cb3c789df841cc281067bca73ff494b6bddd44
Clairine <clairine.christabel@ui.ac.id>
2026-04-21 15:50:27 +0700

    (Bonus) Function improvement

--- modul-6/hello/src/lib.rs
+++ modul-6/hello/src/lib.rs
@@ -11,8 +11,10 @@ pub struct ThreadPool {
 type Job = Box<dyn FnOnce() + Send + 'static>;
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+    pub fn build(size: usize) -> Result<ThreadPool, &'static str> {
+        if size == 0 {
+            return Err("Ukuran thread pool harus lebih besar dari 0.");
+        }
 
         let (sender, receiver) = mpsc::channel();
         let receiver = Arc::new(Mutex::new(receiver));
@@ -22,7 +24,7 @@ impl ThreadPool {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
-        ThreadPool { workers, sender }
+        Ok(ThreadPool { workers, sender })
     }
 
     pub fn execute<F>(&self, f: F)
